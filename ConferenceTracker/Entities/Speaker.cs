d5d0a567710eecd4ac59f33f6de9e6f29b2a0e66
reinaldo558 <reinaldo558@gmail.com>
2023-02-09 15:20:58 +0100

    For Pluralsight

--- ConferenceTracker/Entities/Speaker.cs
+++ ConferenceTracker/Entities/Speaker.cs
@@ -4,18 +4,42 @@ using System.ComponentModel.DataAnnotations;
 
 namespace ConferenceTracker.Entities
 {
-    public class Speaker
+    public class Speaker : IValidatableObject
     {
+
+        [Required()]
         public int Id { get; set; }
         [Display(Name = "First name")]
+        [Required()]
+        [DataType(DataType.Text)]
+        [StringLength(maximumLength:100, MinimumLength = 2)]
         public string FirstName { get; set; }
         [Display(Name = "Last name")]
+        [Required()]
+        [DataType(DataType.Text)]
+        [StringLength(maximumLength: 100, MinimumLength = 2)]
         public string LastName { get; set; }
+        [Required()]
+        [DataType(DataType.MultilineText)]
+        [StringLength(maximumLength: 500, MinimumLength = 10)]
         public string Description { get; set; }
         [Display(Name = "Email Address")]
+        [DataType(DataType.EmailAddress)]
         public string EmailAddress { get; set; }
         [Display(Name = "Phone Number")]
+        [DataType(DataType.PhoneNumber)]
         public string PhoneNumber { get; set; }
         public bool IsStaff { get; set; }
+
+        public IEnumerable<ValidationResult> Validate(ValidationContext validationContext)
+        {
+            List<ValidationResult> results = new List<ValidationResult>();
+            if (EmailAddress != null && EmailAddress.EndsWith("TechnologyLiveConference.com", StringComparison.OrdinalIgnoreCase))
+            {
+                results.Add(new ValidationResult("Technology Live Conference staff should not use their conference email addresses."));
+            }
+
+            return results;
+        }
     }
 }
